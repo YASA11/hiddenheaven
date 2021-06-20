ede55320a7b4a540eb6212ab6bd96b866457ce77
icepankun <76835582+icepankun@users.noreply.github.com>
2021-06-20 15:35:03 +0700

    addfile

---
+++ homepage/js/script.js
@@ -0,0 +1,30 @@
+$(".owl-carousel").owlCarousel({
+    autoplay:true,
+    autoplayhoverpause:true,
+    autoplaytimeout:100,
+    nav:true,
+    loop:true,
+    responsive:{
+        0:{
+            items:1,
+            dots:false
+        },
+        485:{
+            items:1,
+            dots:false
+        },
+        728:{
+            items:2,
+            dots:true
+        },
+        960:{
+            items:3,
+            dots:true
+        },
+        1200:{
+            items:3,
+            dots:true
+        }
+    }
+});
+
